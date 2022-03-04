0aadcae0d560c4f0585f6a4e35618eb3eb009dbc
patrick mugambi <patrick@kali.pm.com>
2022-03-04 10:13:57 +0300

    change database

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,7 +2,6 @@
 import os
 import django_heroku
 import dj_database_url
-from decouple import config,Csv
 
 import dotenv
 
@@ -94,14 +93,9 @@ DATABASES = {
 }
 
 # load environment variables from .env
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
-# load database from the DATABASE_URL environment variable
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config()
-
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
