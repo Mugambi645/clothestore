6a9866194733c59d202394b30ff36a28f7efb6cc
patrick mugambi <patrick@kali.pm.com>
2022-03-05 09:20:27 +0300

    set image field to be served by cloudinary

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from cloudinary.models import CloudinaryField
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -22,7 +22,8 @@ class Product(models.Model):
                                  on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    #image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image = CloudinaryField('image')
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
